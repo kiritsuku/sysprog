0fdb9bf6de19fcea9ad7e776a439104ed0529bcd
Simon Schaefer <mail@antoras.de>
2014-12-07 02:25:21 +0100

    Add nesting generated code + add semicolon after statements

--- Parser/src/TestParser.cpp
+++ Parser/src/TestParser.cpp
@@ -11,27 +11,35 @@
 
 #include <sstream>
 
-void mkString(std::stringstream &ss, Nodes::Node *node)
+void mkNesting(std::stringstream &ss, unsigned nesting)
+{
+  for (unsigned i = 0; i < nesting; ++i)
+    ss << "  ";
+}
+
+void mkString(std::stringstream &ss, Nodes::Node *node, unsigned nesting)
 {
   switch (node->tpe()) {
     case Nodes::Prog:
-      mkString(ss, node->decls());
-      mkString(ss, node->stmts());
+      mkString(ss, node->decls(), nesting);
+      mkString(ss, node->stmts(), nesting);
       break;
 
     case Nodes::Decls:
-      mkString(ss, node->decl());
+      mkString(ss, node->decl(), nesting);
       ss << ";\n";
-      mkString(ss, node->nextDecl());
+      mkString(ss, node->nextDecl(), nesting);
       break;
 
     case Nodes::DeclArray:
+      mkNesting(ss, nesting);
       ss << "int";
-      mkString(ss, node->arr());
+      mkString(ss, node->arr(), nesting);
       ss << " " << node->symbol()->ident;
       break;
 
     case Nodes::DeclIdent:
+      mkNesting(ss, nesting);
       ss << "int " << node->symbol()->ident;
       break;
 
@@ -40,65 +48,74 @@ void mkString(std::stringstream &ss, Nodes::Node *node)
       break;
 
     case Nodes::Statements:
-      mkString(ss, node->stmt());
-      ss << "\n";
-      mkString(ss, node->nextStmt());
+      mkString(ss, node->stmt(), nesting);
+      ss << ";\n";
+      mkString(ss, node->nextStmt(), nesting);
       break;
 
     case Nodes::StatementIdent:
+      mkNesting(ss, nesting);
       ss << node->symbol()->ident;
-      mkString(ss, node->index());
+      mkString(ss, node->index(), nesting);
       ss << " := ";
-      mkString(ss, node->exp());
+      mkString(ss, node->exp(), nesting);
       break;
 
     case Nodes::StatementWrite:
+      mkNesting(ss, nesting);
       ss << "write(";
-      mkString(ss, node->exp());
+      mkString(ss, node->exp(), nesting);
       ss << ")";
       break;
 
     case Nodes::StatementRead:
+      mkNesting(ss, nesting);
       ss << "read(" << node->symbol()->ident;
-      mkString(ss, node->index());
+      mkString(ss, node->index(), nesting);
       ss << ")";
       break;
 
     case Nodes::StatementIf:
+      mkNesting(ss, nesting);
       ss << "if(";
-      mkString(ss, node->exp());
+      mkString(ss, node->exp(), nesting);
       ss << ") ";
-      mkString(ss, node->ifStmt());
+      mkString(ss, node->ifStmt(), nesting);
+      mkNesting(ss, nesting);
       ss << "else ";
-      mkString(ss, node->elseStmt());
+      mkString(ss, node->elseStmt(), nesting);
       break;
 
     case Nodes::StatementWhile:
+      mkNesting(ss, nesting);
       ss << "while(";
-      mkString(ss, node->exp());
+      mkString(ss, node->exp(), nesting);
       ss << ") ";
-      mkString(ss, node->stmt());
+      mkString(ss, node->stmt(), nesting);
       break;
 
     case Nodes::StatementBlock:
+      mkNesting(ss, nesting);
       ss << "{\n";
-      mkString(ss, node->stmts());
-      ss << "\n}\n";
+      mkString(ss, node->stmts(), nesting+1);
+      mkNesting(ss, nesting);
+      ss << "}";
+      break;
 
     case Nodes::Exp:
-      mkString(ss, node->exp2());
-      mkString(ss, node->op());
+      mkString(ss, node->exp2(), nesting);
+      mkString(ss, node->op(), nesting);
       break;
 
     case Nodes::Exp2:
       ss << "(";
-      mkString(ss, node->exp());
+      mkString(ss, node->exp(), nesting);
       ss << ")";
       break;
 
     case Nodes::Exp2Ident:
       ss << node->symbol()->ident;
-      mkString(ss, node->index());
+      mkString(ss, node->index(), nesting);
       break;
 
     case Nodes::Exp2Int:
@@ -107,23 +124,23 @@ void mkString(std::stringstream &ss, Nodes::Node *node)
 
     case Nodes::Exp2Minus:
       ss << "-";
-      mkString(ss, node->exp2());
+      mkString(ss, node->exp2(), nesting);
       break;
 
     case Nodes::Exp2Neg:
       ss << "!";
-      mkString(ss, node->exp2());
+      mkString(ss, node->exp2(), nesting);
       break;
 
     case Nodes::Index:
       ss << "[";
-      mkString(ss, node->exp());
+      mkString(ss, node->exp(), nesting);
       ss << "]";
       break;
 
     case Nodes::OpExp:
-      mkString(ss, node->op());
-      mkString(ss, node->exp());
+      mkString(ss, node->op(), nesting);
+      mkString(ss, node->exp(), nesting);
       break;
 
     case Nodes::Op:
@@ -162,7 +179,7 @@ int main(int argc, char* argv[])
 
   auto node = parser.parse();
   std::stringstream ss;
-  mkString(ss, node);
+  mkString(ss, node, 0);
   printf("%s\n", ss.str().c_str());
   delete node;
 
